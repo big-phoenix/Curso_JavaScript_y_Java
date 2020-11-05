df9e8d5893e6f8220b176bd0da993672da4f5910
Javi <bisistems@gmail.com>
2020-11-05 16:37:46 +0100

    Variables en JavaScript Fase2

--- variables_en_JavaScript/js/variables.js
+++ variables_en_JavaScript/js/variables.js
@@ -14,5 +14,21 @@ function variables(){
     document.getElementById("text1").innerHTML = "Nombre: " +firtName+ " " +seconName+ ", " +name+ "<br>";
     document.getElementById("text2").innerHTML = "Edad: " +day+ "/" +month+ "/" +year+ "<br>";
     
+    /********************************************** Fase2 ***************************************************************/
+
+    const traspas = 1948;
+    let temps = 4;
+    let anys = 0;
+
+
+    for(i = traspas; i <= year; i++) {
+			
+        i += temps;
+        anys++;
+        
+    }
+
+    document.getElementById("text3").innerHTML = "Anys de traspas en " +traspas+ " y " +year+ " son: " +(anys+1)+ "<br>";
+
     
 };
